8fe6ef6f722fbd3f440d25a42a2f18773113a690
degree2466 <uzochukwugodwin959@gmail.com>
2023-05-03 19:32:13 -0700

    task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-# Use the Oniguruma library for regular expressions
-require 'oniguruma'
-
-# Get the argument passed to the script
-argument = ARGV[0]
-
-# Define a regular expression pattern to match against the argument
-pattern = Oniguruma::ORegexp.new('hello', options: Oniguruma::OPTION_IGNORECASE)
-
-# Use the regular expression to match against the argument
-if pattern.match(argument)
-  puts "Argument matches pattern!"
-else
-  puts "Argument does not match pattern."
-end
+puts ARGV[0].scan(/School/).join
