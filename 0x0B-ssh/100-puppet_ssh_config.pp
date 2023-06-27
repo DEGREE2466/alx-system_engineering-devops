95a07cf48fee86805cb047d53b60d623c06d2b46
degree2466 <uzochukwugodwin959@gmail.com>
2023-06-27 04:42:42 -0700

    changes SSH config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Manifest  to change the SSH config
+
+exec {'add_ssh_config':
+command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+path => 'usr/bin:/bin',
+}
