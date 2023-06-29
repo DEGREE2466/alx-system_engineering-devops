547566d688af99b661009928f3a629296185510a
degree2466 <uzochukwugodwin959@gmail.com>
2023-06-28 20:29:49 -0700

    task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+# Installing Nginx package
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Configures Nginx
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => "
+    server {
+      listen 80 default_server;
+      server_name _;
+      location / {
+        return 301 https://smartprofessionals.tech/;
+      }
+      location /redirect_me {
+        return 301 https://www.alxafrica.com/;
+      }
+      location = / {
+        return 200 'Hello World!';
+      }
+    }
+  ",
+  require => Package['nginx'],
+}
+
+# Enables the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'link',
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Restart Nginx service
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
+}
