91f78c7ab547739bc7dcd1fc5c816d8788eeee51
degree2466 <uzochukwugodwin959@gmail.com>
2023-07-24 23:22:33 -0400

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,31 +4,50 @@ import csv
 import requests
 import sys
 
+def get_employee_name(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    user_url = f"{base_url}/{employee_id}"
+
+    try:
+        response = requests.get(user_url)
+        response.raise_for_status()
+        user_info = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: Unable to fetch user information - {e}")
+        return
+
+    return user_info.get('name', 'Unknown Employee')
+
+def get_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    todo_url = f"{base_url}/{employee_id}/todos"
+
+    try:
+        response = requests.get(todo_url)
+        response.raise_for_status()
+        todos = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: Unable to fetch data - {e}")
+        return
+
+    # Count completed tasks and total tasks
+    completed_tasks = [todo for todo in todos if todo['completed']]
+    total_tasks = len(todos)
+
+    # Get employee name
+    employee_name = get_employee_name(employee_id)
+
+    # Display the result in the CSV format
+    print(f"Number of tasks in CSV: {len(completed_tasks)}/{total_tasks}")
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+    import sys
 
-    userid = sys.argv[1]
-    user = '{}users/{}'.format(url, userid)
-    res = requests.get(user)
-    json_o = res.json()
-    name = json_o.get('username')
-
-    todos = '{}todos?userId={}'.format(url, userid)
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        l_task.append([userid,
-                       name,
-                       task.get('completed'),
-                       task.get('title')])
-
-    filename = '{}.csv'.format(userid)
-    with open(filename, mode='w') as employee_file:
-        employee_writer = csv.writer(employee_file,
-                                     delimiter=',',
-                                     quotechar='"',
-                                     quoting=csv.QUOTE_ALL)
-        for task in l_task:
-            employee_writer.writerow(task)
+    if len(sys.argv) != 2:
+        print("Usage: python script.py EMPLOYEE_ID")
+    else:
+        try:
+            employee_id = int(sys.argv[1])
+            get_employee_todo_progress(employee_id)
+        except ValueError:
+            print("Error: Invalid employee ID. Please provide an integer.")
